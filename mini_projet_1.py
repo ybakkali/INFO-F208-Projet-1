91bdea532013bb6105afef42e56c9977e9ae63f7
Yahya Bakkali <33006738+ybakkali@users.noreply.github.com>
2019-10-21 19:52:34 +0000

    Add files via upload

--- mini_projet_1.py
+++ mini_projet_1.py
@@ -1,24 +1,27 @@
 def matrixParser(filename):
-    with open(filename, encoding='utf-8') as f:
-        matrix = SubstitutionMatrix()
-        for line in f:
-            if line[0] not in ["#", " ", "\n"]:
-                matrix.Append(line[0])
-                matrix.addRow(line[1:].split())
+    f = open(filename, encoding='utf-8')
+    matrix = SubstitutionMatrix()
+    for line in f:
+        if line[0] not in ["#", " ", "\n"]:
+            matrix.Append(line[0])
+            matrix.addRow(line[1:].split())
+    f.close()
     return matrix
 
 
 def sequencesParser(filename):
-    with open(filename, encoding='utf-8') as f:
-        mylist = []
-        sequence = ''
-        for line in f:
-            if line[0] not in [">", "\n"]:
-                sequence += line.strip("\n")
-            else:
-                if len(sequence):
-                    mylist.append(ADTsequence(sequence))
-                    sequence = ''
+    f = open(filename, encoding='utf-8')
+    mylist = []
+    sequence = ''
+    for line in f:
+        if line[0] not in [">", " ", "\n"]:
+            sequence += line.strip("\n")
+        elif len(sequence) :
+            mylist.append(ADTsequence(sequence))
+            sequence = ''
+    if len(sequence) :
+        mylist.append(ADTsequence(sequence))
+    f.close()
     return mylist
 
 
@@ -139,44 +142,53 @@ class NeedlemanWunsch(object):
         self.k = k
         self.I = I
         self.E = E
+        self.solutionCounter = 0
 
         for i in range(1, self.S.n):
             for j in range(1, self.S.m):
                 self.fill(i, j)
-        self.findWay()
+        self.findAlignment()
 
-    def findWay(self, i=None, j=None, way=[], ALIGN=[], SOLUTIONS=[]):
-
+    def findAlignment(self, i=None, j=None,seq = "",seq2 = "",seq3 = ""):
         if i is None and j is None:
             i = self.S.n - 1
             j = self.S.m - 1
+        if i >= 0 and j >= 0 and self.solutionCounter < self.k :
+            if i == 0 and j == 0:
+                self.solutionCounter += 1
+                show(self.solutionCounter,self.S.getItem(self.S.n - 1, self.S.m - 1), seq[::-1], seq2[::-1], seq3[::-1])
 
-        value = self.S.getItem(i, j)
-        way.append(value)
+            else:
 
-        if i == 0 and j == 0:
-            if len(SOLUTIONS) < self.k:
-                SOLUTIONS.append((way[::-1], ALIGN[::-1]))
+                if self.S.getItem(i, j) == self.S.getItem(i - 1, j - 1) + self.t(i, j):
+                    seq += self.S.sequence1[i - 1]
+                    seq2 += self.S.sequence2[j - 1]
+                    if self.S.sequence1[i - 1] == self.S.sequence2[j - 1]:
+                        seq3 += "|"
+                    else:
+                        seq3 += ":" if self.t(i, j) >= 0 else "."
+                    self.findAlignment(i - 1, j - 1,seq,seq2,seq3)
+                    seq = seq[:-1]
+                    seq2 = seq2[:-1]
+                    seq3 = seq3[:-1]
 
-        else:
+                if self.S.getItem(i, j) == self.V.getItem(i, j) or j == 0:
+                    seq += self.S.sequence1[i - 1]
+                    seq2 += "-"
+                    seq3 += " "
+                    self.findAlignment(i - 1, j,seq,seq2,seq3)
+                    seq = seq[:-1]
+                    seq2 = seq2[:-1]
+                    seq3 = seq3[:-1]
 
-            if value == self.S.getItem(i - 1, j - 1) + self.t(i, j):
-                ALIGN.append((self.S.sequence1[i - 1], self.S.sequence2[j - 1]))
-                self.findWay(i - 1, j - 1, way, ALIGN)
-                ALIGN.pop()
-
-            if value == self.V.getItem(i, j) or j == 0:
-                ALIGN.append(("-", self.S.sequence2[j - 1]))
-                self.findWay(i - 1, j, way, ALIGN)
-                ALIGN.pop()
-
-            if value == self.W.getItem(i, j) or i == 0:
-                ALIGN.append((self.S.sequence1[i - 1], "-"))
-                self.findWay(i, j - 1, way, ALIGN)
-                ALIGN.pop()
-
-        way.pop()
-        return SOLUTIONS
+                if self.S.getItem(i, j) == self.W.getItem(i, j) or i == 0:
+                    seq += "-"
+                    seq2 += self.S.sequence2[j - 1]
+                    seq3 += " "
+                    self.findAlignment(i, j - 1,seq,seq2,seq3)
+                    seq = seq[:-1]
+                    seq2 = seq2[:-1]
+                    seq3 = seq3[:-1]
 
     def t(self, i, j):
         x = self.MATSUB[self.S.sequence2[j - 1]]
@@ -184,13 +196,10 @@ class NeedlemanWunsch(object):
         return int(self.MATSUB.getItem(x, y))
 
     def fill(self, i, j):
-        Vij = max(self.S.getItem(i - 1, j) - self.I, self.V.getItem(i - 1, j) - self.E)
-        Wij = max(self.S.getItem(i, j - 1) - self.I, self.W.getItem(i, j - 1) - self.E)
-        Sij = max(self.S.getItem(i - 1, j - 1) + self.t(i, j), Vij, Wij)
 
-        self.V.setItem(i, j, Vij)
-        self.W.setItem(i, j, Wij)
-        self.S.setItem(i, j, Sij)
+        self.V.setItem(i, j, max(self.S.getItem(i - 1, j) - self.I, self.V.getItem(i - 1, j) - self.E))
+        self.W.setItem(i, j, max(self.S.getItem(i, j - 1) - self.I, self.W.getItem(i, j - 1) - self.E))
+        self.S.setItem(i, j, max(self.S.getItem(i - 1, j - 1) + self.t(i, j), self.V.getItem(i,j), self.W.getItem(i,j)))
 
 
 class SmithWaterman(object):
@@ -210,7 +219,7 @@ class SmithWaterman(object):
         i = 0
         while i < l and sum([sum(row) for row in self.S.matrix]):
             indexList = []
-            self.topDown(indexList)
+            self.topDown(i+1,indexList)
             self.fillZero(indexList)
             x, y = indexList[-1]
             self.computeAgain(x, y)
@@ -223,13 +232,9 @@ class SmithWaterman(object):
 
     def fill(self, i, j):
 
-        Vij = max(self.S.getItem(i - 1, j) - self.I, self.V.getItem(i - 1, j) - self.E)
-        Wij = max(self.S.getItem(i, j - 1) - self.I, self.W.getItem(i, j - 1) - self.E)
-        Sij = max(self.S.getItem(i - 1, j - 1) + self.t(i, j), Vij, Wij)
-
-        self.V.setItem(i, j, max(0, Vij))
-        self.W.setItem(i, j, max(0, Wij))
-        self.S.setItem(i, j, max(0, Sij))
+        self.V.setItem(i, j, max(0, self.S.getItem(i - 1, j) - self.I, self.V.getItem(i - 1, j) - self.E))
+        self.W.setItem(i, j, max(0, self.S.getItem(i, j - 1) - self.I, self.W.getItem(i, j - 1) - self.E))
+        self.S.setItem(i, j, max(0, self.S.getItem(i - 1, j - 1) + self.t(i, j), self.V.getItem(i,j), self.W.getItem(i,j)))
 
     def maxValue(self):
 
@@ -242,7 +247,7 @@ class SmithWaterman(object):
                     x, y = i, j
         return x, y
 
-    def topDown(self, indexList):
+    def topDown(self, number, indexList):
 
         i, j = self.maxValue()
         indexList.append((i, j))
@@ -279,7 +284,7 @@ class SmithWaterman(object):
 
             indexList.append((i, j))
 
-        show(score, seq[::-1], seq2[::-1], seq3[::-1],pos,pos2)
+        show(number, score, seq[::-1], seq2[::-1], seq3[::-1],pos,pos2)
         indexList.pop()
 
     def fillZero(self, indexList):
@@ -299,13 +304,20 @@ class SmithWaterman(object):
             i += 1
             j = y
 
-def show(score, sequence, sequence2, sequence3,pos,pos2):
-    for i in range(len(sequence) // 60 + 1):
-        print(sequence[i * 60:(i + 1) * 60] \
-            + " - from {0} to {1}".format(pos2[0],pos[0]) \
-            + "\n" + sequence3[i * 60:(i + 1) * 60] \
-            + "\n" + sequence2[i * 60:(i + 1) * 60] \
-            + " - from {0} to {1}".format(pos2[1],pos[1]) +"\n")
+def show(number, score, sequence, sequence2, sequence3, pos=None, pos2=None):
+    print("Soluntion n°: {0} ".format(number))
+    w = (len(sequence) // 60 + 1) if len(sequence) % 60 != 0 else len(sequence) // 60
+    for i in range(w):
+        if pos is None and pos2 is None :
+            print(sequence[i * 60:(i + 1) * 60] \
+                + "\n" + sequence3[i * 60:(i + 1) * 60] \
+                + "\n" + sequence2[i * 60:(i + 1) * 60] + "\n")
+        else :
+            print(sequence[i * 60:(i + 1) * 60] \
+                + " - from {0} to {1}".format(pos2[0],pos[0]) \
+                + "\n" + sequence3[i * 60:(i + 1) * 60] \
+                + "\n" + sequence2[i * 60:(i + 1) * 60] \
+                + " - from {0} to {1}".format(pos2[1],pos[1]) + "\n")
 
     print("Score :", score)
     print("Identity percentage :", round(sequence3.count("|") / len(sequence3) * 100,2), "%")
@@ -316,17 +328,19 @@ MATSUB = matrixParser("pam120.txt")
 sequences = sequencesParser("WW-sequence.fasta")
 for i in range(len(sequences)):
     for j in range(i+1,len(sequences)) :
-            print("Alignment between sequence {0} and {1}".format(i+1,j+1))
-            S = ScoringMatrix("S","GLOBAL",sequences[i],sequences[j],6,3)
-            V = ScoringMatrix("V","GLOBAL",sequences[i],sequences[j])
-            W = ScoringMatrix("W","GLOBAL",sequences[i],sequences[j])
-            NeedlemanWunsch(S,V,W,MATSUB,5,6,3)
+        print("\n" * 2)
+        print("Alignment between sequence {0} and {1}".format(i+1,j+1))
+        S = ScoringMatrix("S","GLOBAL",sequences[i],sequences[j],7,3)
+        V = ScoringMatrix("V","GLOBAL",sequences[i],sequences[j])
+        W = ScoringMatrix("W","GLOBAL",sequences[i],sequences[j])
+        NeedlemanWunsch(S,V,W,MATSUB,9,7,3)
 
+MATSUB = matrixParser("blosum80.txt")
 sequences = sequencesParser("protein-sequences.fasta")
 for i in range(len(sequences)):
     for j in range(i+1,len(sequences)) :
             print("Alignment between sequence {0} and {1}".format(i+1,j+1))
-            S = ScoringMatrix("S","LOCAL",sequences[i],sequences[j],8,1)
+            S = ScoringMatrix("S","LOCAL",sequences[i],sequences[j],5,1)
             V = ScoringMatrix("V","LOCAL",sequences[i],sequences[j])
             W = ScoringMatrix("W","LOCAL",sequences[i],sequences[j])
-            SmithWaterman(S, V, W, MATSUB, 3, 8, 1)
+            SmithWaterman(S, V, W, MATSUB, 8, 5, 1)
